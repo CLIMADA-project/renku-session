697097f6c39826c8d4a33958c35876b12eda7168
emanuel-schmid <schmide@ethz.ch>
2026-09-09 11:42:11 +0200

    add climada.conf file and setup script

---
+++ init-impact-forecasting.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/bash
+
+WD=/home/renku/work
+
+echo install climada
+cd $WD/climada_python
+git checkout develop
+pip install -e .
+
+echo configure local data for climada
+cp $WD/renku-session/climada.config $WD/
+
+echo install impact-forecasting-warning
+cd $WD/impact-forecasting-warning
+git checkout renku-env
+pip install -e
+
+echo provide gpw input file
+mkdir $WD/climada/data -p
+unzip -u *-zenodo.*/*.zip -d $WD/climada/data
+
+echo all set up. you\'re ready to go
+echo try: \`python -m impact_forecasting_warning.pipelines.wind_impact_forecast --n-days 1\`
